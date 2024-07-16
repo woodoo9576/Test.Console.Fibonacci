5b5bb6dc29177400c0c91a00e15e1498afec11d7
Adem Balaban <woodoo9576@hotmail.com>
2024-07-16 09:41:25 +0300

    Area

--- Test.Console/Program.cs
+++ Test.Console/Program.cs
@@ -8,11 +8,29 @@ namespace Test.Console
 
     class Program
     {
-        
+        public class Rectangle
+        {
+            public Rectangle(int height, int width) => (Height, Width) = (height, width);
+
+            public int Width { get; }
+            public int Height { get;}
+
+            public int Area => Width * Height;
+            public void DisplayArea()=> Console.WriteLine($"Area: {Area}");
+        }
         static void Main(string[] args)
         {
-            Fibonacci( 1000, 1020);
-
+            /*
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Başlangıç Sayısını Giriniz: ");
+            int? a = int?.Parse(Console.ReadLine());
+            Console.WriteLine("Bitiş Sayısını Giriniz: ");
+            int? b = int?.Parse(Console.ReadLine()!);
+            
+            Fibonacci(a ?? 10, b ?? 20);
+            */
+            Rectangle rectangle = new Rectangle(10, 20);
+            rectangle.DisplayArea();
             void Fibonacci(BigInteger x, BigInteger y)
             {
                 BigInteger c = 0;
@@ -20,7 +38,7 @@ namespace Test.Console
                 BigInteger a = 0;
                 for (BigInteger i = 0; i <= y; i++)
                 {
-                    c= a + b;
+                    c = a + b;
                     a = b;
                     b = c;
                     if (i >= x && i < y)
